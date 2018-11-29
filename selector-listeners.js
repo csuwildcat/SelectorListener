0a6ce1e8599a3cedb6de658adbcb701d421875ae
Daniel Buchner <danieljb2@gmail.com>
2018-11-29 11:04:35 -0800

    Updated with @rule support

--- selector-listeners.js
+++ selector-listeners.js
@@ -1,81 +1,60 @@
 (function(){
 	
 	var events = {},
-		selectors = {},
-		animationCount = 0,
-		styles = document.createElement('style'),
-		keyframes = document.createElement('style'),
-		head = document.getElementsByTagName('head')[0],
-		startNames = ['animationstart', 'oAnimationStart', 'MSAnimationStart', 'webkitAnimationStart'],
-		startEvent = function(event){
-			event.selector = (events[event.animationName] || {}).selector;
-			((this.selectorListeners || {})[event.animationName] || []).forEach(function(fn){
-				fn.call(this, event);
-			}, this);
-		},
-		prefix = (function() {
-			var duration = 'animation-duration: 0.001s;',
-				name = 'animation-name: SelectorListener !important;',
-				computed = window.getComputedStyle(document.documentElement, ''),
-				pre = (Array.prototype.slice.call(computed).join('').match(/moz|webkit|ms/)||(computed.OLink===''&&['o']))[0];
-			return {
-				css: '-' + pre + '-',
-				properties: '{' + duration + name + '-' + pre + '-' + duration + '-' + pre + '-' + name + '}',
-				keyframes: !!(window.CSSKeyframesRule || window[('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1] + 'CSSKeyframesRule'])
-			};
-		})();
-		
-	styles.type = keyframes.type = "text/css";
-	head.appendChild(styles);
-	head.appendChild(keyframes);
-	
-	HTMLDocument.prototype.addSelectorListener = HTMLElement.prototype.addSelectorListener = function(selector, fn){
-		var key = selectors[selector],
-			listeners = this.selectorListeners = this.selectorListeners || {};
+      selectors = {},
+      animationCount = 0,
+      styles = document.createElement('style'),
+      keyframes = document.head.appendChild(document.head.appendChild(styles).cloneNode()),
+      prefix = window.CSSKeyframesRule ? { css: '', event: 'animationstart' } : { css: '-webkit-', event: 'webkitAnimationStart' },
+      startEvent = function(e){
+        e.selector = (events[e.animationName] || {}).selector;
+        ((this.selectorListeners || {})[e.animationName] || []).forEach(function(fn) { fn.call(this, e) }, this);
+      }
+  
+	HTMLDocument.prototype.addSelectorListener = HTMLElement.prototype.addSelectorListener = function(exp, fn){
+    var atRule = typeof exp === 'string' ? '' : exp[0];
+    var selector = atRule ? exp[1] : exp;
+    var exp = atRule ? exp.join(' ') : exp;
+		var key = selectors[exp],
+			  listeners = this.selectorListeners = this.selectorListeners || { count: 0 };
 			
 		if (key) events[key].count++;
 		else {
-			key = selectors[selector] = 'SelectorListener-' + animationCount++;
-			var node = document.createTextNode('@' + (prefix.keyframes ? prefix.css : '') + 'keyframes ' + key + ' {'
-				+'from { outline-color: #fff; } to { outline-color: #000; }'
-			+ '}');
-			keyframes.appendChild(node);
-			styles.sheet.insertRule(selector + prefix.properties.replace(/SelectorListener/g, key), 0);
-			events[key] = { count: 1, selector: selector, keyframe: node, rule: styles.sheet.cssRules[0] };
+			key = selectors[exp] = 'SelectorListener-' + animationCount++;
+      var style = selector + '::before { content: ""; ' + prefix.css + 'animation: ' + key + '}';
+			styles.sheet.insertRule(atRule ? atRule + '{' + style + '}' : style, 0);
+			events[key] = {
+        count: 1,
+        selector: exp,
+        keyframe: keyframes.appendChild(document.createTextNode('@' + prefix.css + 'keyframes ' + key + ' { to { --selector-listeners: 1 } }')),
+        rule: styles.sheet.cssRules[0]
+      };
 		} 
 		
-		if (listeners.count) listeners.count++;
-		else {
-			listeners.count = 1;
-			startNames.forEach(function(name){
-				this.addEventListener(name, startEvent, false);
-			}, this);
-		}
+		listeners.count ? listeners.count++ : this.addEventListener(prefix.event, startEvent, true);
 		
 		(listeners[key] = listeners[key] || []).push(fn);
 	};
 	
-	HTMLDocument.prototype.removeSelectorListener = HTMLElement.prototype.removeSelectorListener = function(selector, fn){
+	HTMLDocument.prototype.removeSelectorListener = HTMLElement.prototype.removeSelectorListener = function(exp, fn){
+    var exp = typeof exp === 'string' ? exp : exp.join(' ') ;
 		var listeners = this.selectorListeners || {},
-			key = selectors[selector],
-			listener = listeners[key] || [],
-			index = listener.indexOf(fn);
+			  key = selectors[exp],
+			  listener = listeners[key] || [],
+			  index = listener.indexOf(fn);
 			
 		if (index > -1){
-			var event = events[selectors[selector]];
-			event.count--;
-			if (!event.count){
+			var event = events[selectors[exp]];
+			if (!--event.count){
 				styles.sheet.deleteRule(styles.sheet.cssRules.item(event.rule));
 				keyframes.removeChild(event.keyframe);
 				delete events[key];
-				delete selectors[selector];
+				delete selectors[exp];
 			}
 			
 			listeners.count--;
 			listener.splice(index, 1);
-			if (!listeners.count) startNames.forEach(function(name){
-				this.removeEventListener(name, startEvent, false);
-			}, this);
+			if (!listeners.count) this.removeEventListener(prefix.event, startEvent);
 		}
 	};
 	
